3be097320ee445d99a042e266d4c55032e999f20
Gregory John Casamento <greg.casamento@gmail.com>
2025-06-03 03:32:41 -0400

    Add initial files

---
+++ AppController.h
@@ -0,0 +1,43 @@
+/* 
+   Project: VideoPlayer
+
+   Author: Gregory John Casamento,,,
+
+   Created: 2025-06-03 03:09:42 -0400 by heron
+   
+   Application Controller
+*/
+ 
+#ifndef _PCAPPPROJ_APPCONTROLLER_H
+#define _PCAPPPROJ_APPCONTROLLER_H
+
+#import <AppKit/AppKit.h>
+// Uncomment if your application is Renaissance-based
+//#import <Renaissance/Renaissance.h>
+
+@interface AppController : NSObject
+{
+  IBOutlet NSWindow *_window;
+}
+
+// Class methods...
++ (void)  initialize;
+
+// Initialization
+- (id) init;
+- (void) dealloc;
+- (void) awakeFromNib;
+
+// Notification methods...
+- (void) applicationDidFinishLaunching: (NSNotification *)aNotif;
+- (BOOL) applicationShouldTerminate: (id)sender;
+- (void) applicationWillTerminate: (NSNotification *)aNotif;
+- (BOOL) application: (NSApplication *)application
+	    openFile: (NSString *)fileName;
+
+// Actions...
+- (IBAction) showPrefPanel: (id)sender;
+
+@end
+
+#endif
---
+++ VideoPlayer_main.m
@@ -0,0 +1,30 @@
+/* 
+   Project: VideoPlayer
+
+   Author: Gregory John Casamento,,,
+
+   Created: 2025-06-03 03:09:42 -0400 by heron
+*/
+
+#import <AppKit/AppKit.h>
+
+int 
+main(int argc, const char *argv[])
+{
+// Uncomment if your application is Renaissance application
+/*  CREATE_AUTORELEASE_POOL (pool);
+  [NSApplication sharedApplication];
+  [NSApp setDelegate: [AppController new]];
+
+  #ifdef GNUSTEP
+    [NSBundle loadGSMarkupNamed: @"MainMenu-GNUstep"  owner: [NSApp delegate]];
+  #else
+    [NSBundle loadGSMarkupNamed: @"MainMenu-OSX"  owner: [NSApp delegate]];
+  #endif
+   
+  RELEASE (pool);
+*/
+
+  return NSApplicationMain (argc, argv);
+}
+
